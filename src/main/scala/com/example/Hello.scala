58022a4e0488b202bd4434918f7b8b488528ce44
Rafa Paradela <rafa.p@47deg.com>
2015-10-16 12:49:41 +0200

    Added osciloscope 01

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -3,25 +3,26 @@ package com.example
 import org.scalajs.dom.raw.HTMLCanvasElement
 import scala.scalajs.js.annotation.JSExport
 import org.scalajs.dom
-import org.scalajs.dom.html
+import org.scalajs.dom.{CanvasRenderingContext2D, html}
 
 @JSExport
 object DemoExamples  {
   @JSExport
   def main(canvas: html.Canvas): Unit = {
     val ctx = canvas.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
-    dom.window.addEventListener("resize", { e: dom.Event => resizeCanvas(ctx.canvas) }, false)
-    resizeCanvas(ctx.canvas)
+    dom.window.addEventListener("resize", { e: dom.Event => resizeCanvas(ctx) }, false)
+    resizeCanvas(ctx)
   }
 
-  def drawStuff(canvas: HTMLCanvasElement) {
-
+  def drawStuff(ctx: CanvasRenderingContext2D) {
+    ctx.fillStyle = "#FF9900"
+    ctx.fillRect(0, 0, 30, 30)
   }
 
-  def resizeCanvas(canvas: HTMLCanvasElement) = {
-    canvas.width = dom.window.innerWidth
-    canvas.height = dom.window.innerHeight
-    drawStuff(canvas)
+  def resizeCanvas(ctx: CanvasRenderingContext2D) = {
+    ctx.canvas.width = dom.window.innerWidth
+    ctx.canvas.height = dom.window.innerHeight
+    drawStuff(ctx)
   }
 
 }
